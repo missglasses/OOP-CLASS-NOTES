eb11e03b35f4c679dc3b92be02d710e75ee21d21
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-27 10:29:39 +0800

    Update day2-notes1.cs

--- Semester/day2-notes1.cs
+++ Semester/day2-notes1.cs
@@ -7,7 +7,7 @@ class Program { //classname should also be the filename (before)
 
     static void Main(){
       Console.WriteLine("Hello from the Main method!");
-      string name = Console.ReadLine();
+      string name = Console.ReadLine(); //reads strings only
       Console.WriteLine("Hello user!, " +name);
     }
 
