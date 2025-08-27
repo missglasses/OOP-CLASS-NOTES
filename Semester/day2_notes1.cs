e68dbeaee3119f35c570f53165e705a1c67ca125
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-27 10:27:54 +0800

    Create day2_notes1.cs

---
+++ Semester/day2_notes1.cs
@@ -0,0 +1,18 @@
+// See https://aka.ms/new-console-template for more information
+
+
+using System; //library
+
+class Program { //classname should also be the filename (before)
+
+    static void Main(){
+      Console.WriteLine("Hello from the Main method!");
+      string name = Console.ReadLine();
+      Console.WriteLine("Hello user!, " +name);
+    }
+
+
+
+}
+
+
