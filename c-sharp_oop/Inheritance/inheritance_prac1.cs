3b97cdf9bcc78790166fd4684b3dcff0c59a35cd
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-09-01 10:36:22 +0800

    Update inheritance_prac1.cs

--- c-sharp_oop/Inheritance/inheritance_prac1.cs
+++ c-sharp_oop/Inheritance/inheritance_prac1.cs
@@ -1,48 +1,70 @@
 using System;
 
-namespace InheritancePrac { //this is like a folder
-    
-    class Pet { //base class 
-    public string Kind;
-    public string Name;
-    
-     public void display() {
-      Console.WriteLine("I am a pet");
+namespace InheritancePrac //assume this is a folder
+{
+    class Pet //base class
+    {
+        public string Kind;
+        public string Name;
+
+        public virtual void Display()
+        {
+            Console.WriteLine("I am a pet");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Nomnom!!!");
+        }
     }
-}
 
-class Dog:Pet { 
-    public void display() {
-    Console.WriteLine("My name is "+Name);
-    Console.WriteLine("I am a "+Kind);
+    class Dog : Pet //subclass
+    {
+
+        public override void Display()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("I am a " + Kind);
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine($"Arf arf! {Name} is barking");
+        }
     }
-}
 
-class Cat:Pet {
-    public void display(){
-        Console.WriteLine("My name is "+Name);
-    Console.WriteLine("I am a "+Kind);
+    class Cat : Pet //subclass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("I am a " + Kind);
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"Meowww! Purrrr {Name} meows");
+        }
     }
-}
 
+    class Program 
+    {
+        static void Main()
+        {
+            Dog pomeranian = new Dog();
 
-class Program {
-    
-    static void Main(){
-        Dog pomeranian = new Dog();
-        
-        pomeranian.Kind="Pomeranian";
-        pomeranian.Name="Bonbon";
-        pomeranian.display();
-        
-        Console.WriteLine();
-        
-        Cat siamese = new Cat();
-        siamese.Kind="Siamese";
-        siamese.Name="Ana";
-        siamese.display();
-        
+            pomeranian.Kind = "Pomeranian";
+            pomeranian.Name = "Bonbon";
+            pomeranian.Display();
+            pomeranian.Bark();
+
+            Console.WriteLine();
+
+            Cat siamese = new Cat();
+            siamese.Kind = "Siamese";
+            siamese.Name = "Ana";
+            siamese.Display();
+            siamese.Meow();
+        }
     }
-}
-
 }
